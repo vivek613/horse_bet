570b90778cf50a807f5fd33b5ef8afd5715d7174
sumersinghharawat <info.sumersinghharawat@gmail.com>
2024-08-30 10:45:43 +0400

    Live

--- src/Components/User/UserSide/Dashboard.jsx
+++ src/Components/User/UserSide/Dashboard.jsx
@@ -104,7 +104,7 @@ export const Dashboard = () => {
       try {
         await axios
           .get(
-            `https://horse-batting.onrender.com/api/getliveData?id=${data?.uid}&streamId=${id}`
+            `https://horse-bet.onrender.com/api/getliveData?id=${data?.uid}&streamId=${id}`
           )
           .then((res) => {
             console.log("ress", res);
