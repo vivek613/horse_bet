2e0ec3e83558dc52c26b3638053c1b99acb225d1
sumersinghharawat <info.sumersinghharawat@gmail.com>
2024-11-12 09:09:48 +0530

    Done

--- src/Components/User/UserSide/Dashboard.jsx
+++ src/Components/User/UserSide/Dashboard.jsx
@@ -158,7 +158,7 @@ export const Dashboard = () => {
         <div style={{
           color: "#000000"
         }}>
-        For Support What's App with your Registered Email id / Mobile Number to <a href="tel:+918669646969">86 69 64 69 69</a> / <a href="tel:+918669656969">86 69 65 69 69</a> & For Cricket 🏏 Football ⚽ Tennis 🎾 etc. Live Casino make your ID Now for free <a href="https://www.shrijibook.com" target="_blank">www.shrijibook.com</a>
+        For Support What's App with your Registered Email id / Mobile Number to <a href="tel:+918669646969">86 69 64 69 69</a> / <a href="tel:+918669656969">86 69 65 69 69</a>
       </div>
 
       </marquee>
