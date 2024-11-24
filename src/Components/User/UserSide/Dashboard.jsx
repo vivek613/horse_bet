a522ce7002f852a445a81646513f770ededc8107
sumersinghharawat <info.sumersinghharawat@gmail.com>
2024-11-24 10:49:13 +0400

    new update for payment number

--- src/Components/User/UserSide/Dashboard.jsx
+++ src/Components/User/UserSide/Dashboard.jsx
@@ -155,11 +155,16 @@ export const Dashboard = () => {
       <Toaster position="top-right" reverseOrder={false} />
       <marquee className={styles["marq"]} bgcolor="#cdc6eb"
         direction="left" loop="1">
-        <div style={{
-          color: "#000000"
-        }}>
-        For Support What's App with your Registered Email id / Mobile Number to <a href="tel:+918669646969">86 69 64 69 69</a> / <a href="tel:+918669656969">86 69 65 69 69</a>
-      </div>
+        <p>
+        <span style={{ color: '#FF0000', fontWeight: 'bold' }}>🚨 IMPORTANT ANNOUNCEMENT 🚨 </span>
+        <span style={{ color: '#000000' }}>Due to an issue with WhatsApp, kindly use the following alternatives: </span>
+        <span style={{ color: '#007BFF', fontWeight: 'bold' }}>For Communication: </span>
+        <span style={{ color: '#FF4500' }}>8669 646969</span> or <span style={{ color: '#FF4500' }}>8669 656969</span>. 
+        <span style={{ color: '#007BFF', fontWeight: 'bold' }}>For Deposits: </span>
+        <span style={{ color: '#000000' }}>Use GPay or PhonePe to</span>
+        <span style={{ color: '#28A745' }}>9226 492672</span>. 
+        <span style={{ color: '#FF0000', fontWeight: 'bold' }}>We apologize for the inconvenience caused.</span>
+      </p>
 
       </marquee>
       <div className={styles["user-race-data-main"]}>
